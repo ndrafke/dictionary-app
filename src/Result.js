aa628d72b209f6f34aec9282dec5baef553b0b14
ndrafke <nickdrafke@gmail.com>
2021-10-27 10:52:05 -0500

    changed card minWidth

--- src/Result.js
+++ src/Result.js
@@ -7,7 +7,7 @@ export default function Result({result}) {
 
 
     return (
-    <Card  className="m-2" style={{maxWidth: "90vw"}}>
+    <Card  className="m-2" style={{maxWidth: "90vw", minWidth: "50vw"}}>
         <Card.Body>
         <div className="d-flex flex-column justify-content-between">
         <Card.Header border="secondary" className="d-flex align-items-center p-1"  >
@@ -17,7 +17,7 @@ export default function Result({result}) {
         </Card.Header>
         {result.results.filter((info, index) => index < 8).sort((a, b) => a.partOfSpeech > b.partOfSpeech ? 1 : -1).map((info, index) => {
             return (
-                <ul key={index}>
+                <ul key={index} id="result-list">
                
                 <Card.Text className="m-2">
        <li style={{listStyle: "none", fontFamily: "lucinda console"}}>
